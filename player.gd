41576e3c3942ca759a2c64f1a8d1ca25d5eab5ac
RoaKoi <olindavis09@gmail.com>
2026-06-19 10:32:04 -0400

    completely reorganized game
    kaboom

---
+++ player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+# sets initial speed to 100.0
+var SPEED = 100.0
+# function is built into godot, used to define movement to arrow keys. UI_[direction] is arrow keys
+func _physics_process(delta: float) -> void:
+	
+	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	# if statement defines sprinting on Z and changes speed accordingly
+	if Input.is_key_pressed(KEY_Z):
+		SPEED = 175.0
+	else:
+		SPEED = 100.0
+	
+	velocity = input_vector * SPEED
+	move_and_slide()
