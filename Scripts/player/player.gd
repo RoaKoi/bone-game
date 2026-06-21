5090bafbd0347d68844bbaf6e80e2c5dbd2605a1
Olin Blanchard <olindavis09@gmail.com>
2026-06-21 13:04:20 -0400

    reworked movement code to work on all systems
    previous method of getting vector had issues on certain systems, completely reworked the movement code.

--- Scripts/player/player.gd
+++ Scripts/player/player.gd
@@ -6,12 +6,24 @@ var SPEED = 100.0
 @warning_ignore("unused_parameter")
 func _physics_process(delta: float) -> void:
 	if GameState.canMove == true:
-		var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-		# if statement defines sprinting on Z and changes speed accordingly
+		var velocity = Vector2.ZERO # The player's movement vector.
+		if Input.is_action_pressed("move_right"):
+			velocity.x += 1
+		if Input.is_action_pressed("move_left"):
+			velocity.x -= 1
+		if Input.is_action_pressed("move_down"):
+			velocity.y += 1
+		if Input.is_action_pressed("move_up"):
+			velocity.y -= 1
+		
 		if Input.is_action_pressed("sprint"):
-			SPEED = 175.0
+			SPEED = 175
 		else:
-			SPEED = 100.0
-		velocity = input_vector * SPEED
+			SPEED = 100
+		
+		if velocity.length() > 0:
+			velocity = velocity.normalized() * SPEED
+		
+		position += velocity * delta
 	
 	move_and_slide()
